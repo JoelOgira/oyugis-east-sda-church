1342e5aa05db2b2cacd1d688fe1606aab9fc8bc8
Jay <joelogira@gmail.com>
2023-11-11 21:47:36 +0300

    Added various components for rendering

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import CallToAction from "./components/CallToAction";
+import SermonsSnippet from "./components/SermonsSnippet";
+import Showcase from "./components/Showcase";
+import Tithes from "./components/Tithes";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-
+      <CallToAction />
+      <Showcase />
+      <Tithes />
+      <SermonsSnippet />
     </main>
   )
 }
