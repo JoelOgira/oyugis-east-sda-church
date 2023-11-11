663b39b8e9242a68543afebfa24c3db368ddbc70
Jay <joelogira@gmail.com>
2023-11-11 21:48:18 +0300

    Created CTA component

---
+++ app/components/CallToAction.jsx
@@ -0,0 +1,12 @@
+export default function CallToAction() {
+    return (
+        <div className="px-3 py-16 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4">
+            <div className="flex flex-col justify-center items-center text-center space-y-5">
+                <h2 className="text-md font-semibold text-darkBlue pb-6 sm:text-2xl lg:text-3xl">Seek the Lord while he may be found - Isaiah 55:6 <br />
+                    Join us every Sabbath and let’s delve together into the Word of God, and in praise and worship.
+                </h2>
+                <hr className="border-2 border-darkBlue w-1/2" />
+            </div>
+        </div>
+    )
+}
