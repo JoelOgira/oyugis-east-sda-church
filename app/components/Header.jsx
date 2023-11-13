0f7b8ef662fefd8a5ce30263bc3e9c68075ae8f2
Jay <joelogira@gmail.com>
2023-11-13 08:34:05 +0300

    Updated the header component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,12 @@
 "use client"
 
-// import logo from "../assets/logos/logos.png";
-import Link from 'next/link';
-import { useState } from 'react';
-// import { MdOutlineKeyboardArrowUp } from 'react-icons/md';
-// import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+
+import Link from 'next/link'
+import { useState } from 'react'
+import Image from 'next/image'
+import logo from '../../public/logo_sda.png'
+import { MdOutlineKeyboardArrowUp } from 'react-icons/md'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 
 export default function Header() {
 
@@ -31,54 +33,55 @@ export default function Header() {
     }
 
     return (
-        <nav className={`${isOpen ? '' : 'sticky'} bg-opacity-95 z-30 w-full top-0 shadow-md px-3 py-6 md:px-0 lg:sticky`}>
+        <nav className={`${isOpen ? '' : 'sticky'} bg-gradient bg-opacity-95 z-30 w-full top-0 shadow-md px-3 py-4 md:px-0 lg:sticky`}>
             <div className="flex items-center space-x-10 justify-between md:container md:w-5/6 md:mx-auto lg:w-3/4">
-                <Link href="/" className="flex items-center space-x-2">
-                    {/* <img src={logo} alt="Site Icon" className="w-[70px] xl:w-[100px]" /> */}
-                    <div className="hidden xl:flex xl:flex-col xl:[&>p]:text-white xl:[&>p]:text-[0.70rem] xl:[&>p]:underline">
-                        <p className="max-w-[8rem]">Seventh-day</p>
-                        <p className="max-w-[8rem]">Adventist Church</p>
-                        <p className="max-w-[8rem]">Oyugis East</p>
-                    </div>
+                <Link href="/" className="xl:border-2 xl:border-white xl:rounded">
+                    <span className="flex items-center space-x-2 xl:border-[1.5px] xl:border-black xl:rounded">
+                        <Image src={logo} alt="Site Icon" className="w-[40px] xl:w-[45px]" />
+                        <div className="hidden xl:flex xl:flex-col xl:[&>p]:text-white xl:[&>p]:text-[0.60rem] xl:[&>p]:underline">
+                            <p className="max-w-[8rem]">Seventh-day</p>
+                            <p className="max-w-[8rem]">Adventist Church</p>
+                            <p className="max-w-[8rem]">Oyugis East</p>
+                        </div>
+                    </span>
                 </Link>
 
                 <div className="hidden lg:flex lg:flex-wrap lg:items-center lg:justify-center space-x-6 font-thin text-white">
-                    <Link href="/" className="hover:text-lightBlue">Home</Link>
+                    <Link href="/" className="hover:text-gray-400">Home</Link>
 
                     <div onMouseEnter={handleAbout} onMouseLeave={handleAbout} className="cursor-pointer">
-                        <div className="hover:text-lightBlue flex items-center space-x-1">
+                        <div className="hover:text-gray-400 flex items-center space-x-1">
                             <span>About Us</span>
-                            <span>{isAboutClosed ? "+" : "-"}</span>
+                            <span>{isAboutClosed ? <MdOutlineKeyboardArrowUp /> : <MdOutlineKeyboardArrowDown />}</span>
                         </div>
                         {isAboutClosed ? '' :
                             <div className="z-50">
-                                <div className="bg-black absolute pr-9 pl-3 py-3 flex flex-col space-y-3 shadow-md hover:[&>a]:text-lightBlue">
+                                <div className="bg-gradient absolute pr-9 pl-3 py-3 flex flex-col space-y-3 shadow-md hover:[&>a]:text-gray-400">
                                     <Link onClick={handleAbout} href="/about" >Our Church</Link>
                                     <Link onClick={handleAbout} href="/about/beliefs" >Our Beliefs</Link>
                                 </div>
                             </div>}
                     </div>
 
-                    <Link href="/ministries" className="hover:text-lightBlue">Ministries</Link>
-                    <Link href="/bulletins" className="hover:text-lightBlue">Bulletins</Link>
-                    <Link href="/resources" className="hover:text-lightBlue">Resources</Link>
+                    <Link href="/ministries" className="hover:text-gray-400">Ministries</Link>
+                    <Link href="/bulletins" className="hover:text-gray-400">Bulletins</Link>
+                    <Link href="/resources" className="hover:text-gray-400">Resources</Link>
 
                     <div onMouseEnter={handleMedia} onMouseLeave={handleMedia} className="cursor-pointer">
-                        <div className="hover:text-lightBlue flex items-center space-x-1">
+                        <div className="hover:text-gray-400 flex items-center space-x-1">
                             <span>Media</span>
-                            <span>{isMediaClosed ? "+" : "-"}</span>
+                            <span>{isMediaClosed ? <MdOutlineKeyboardArrowUp /> : <MdOutlineKeyboardArrowDown />}</span>
                         </div>
                         {isMediaClosed ? '' :
                             <div className="z-50">
-                                <div className="bg-black absolute pr-9 pl-3 py-3 flex flex-col space-y-3 shadow-md hover:[&>a]:text-lightBlue">
+                                <div className="bg-gradient absolute pr-9 pl-3 py-3 flex flex-col space-y-3 shadow-md hover:[&>a]:text-gray-400">
                                     <Link onClick={handleMedia} href="/media" >Our Music</Link>
                                     <Link onClick={handleMedia} href="/media/sermons" >Live Sermons</Link>
-                                    <Link onClick={handleMedia} href="/media/gallery" >Photo Gallery</Link>
                                 </div>
                             </div>}
                     </div>
 
-                    <Link href="/contact" className="hover:text-lightBlue">Contact Us</Link>
+                    <Link href="/contact" className="hover:text-gray-400">Contact Us</Link>
                 </div>
 
                 <button
@@ -94,18 +97,18 @@ export default function Header() {
             </div>
 
             <div className="lg:hidden">
-                <div id="menu" className={`flex-col ${isHidden ? 'hidden' : 'flex'} absolute place-items-start self-end py-8 mt-3  font-thin bg-opacity-[0.98] text-white rounded bg-secondary sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}>
+                <div id="menu" className={`flex-col ${isHidden ? 'hidden' : 'flex'} absolute place-items-start self-end py-8 mt-3  font-thin bg-opacity-[0.98] text-white rounded bg-gradient-to-b from-secondary to-tertiary sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}>
                     <div className="mx-auto flex flex-col space-y-6">
                         <Link onClick={() => { handleOpen(); handleHidden() }} href="/">Home</Link>
 
                         <div onClick={handleAbout} className="cursor-pointer">
-                            <div className="hover:text-lightBlue flex items-center space-x-1">
+                            <div className="hover:text-gray-400 flex items-center space-x-1">
                                 <span>About Us</span>
-                                <span>{isAboutClosed ? "+" : "-"}</span>
+                                <span>{isAboutClosed ? <MdOutlineKeyboardArrowUp /> : <MdOutlineKeyboardArrowDown />}</span>
                             </div>
                             {isAboutClosed ? '' :
                                 <div className="z-50">
-                                    <div className="text-gray-200 relative pt-2 flex flex-col space-y-4 hover:[&>a]:text-lightBlue [&>a]:border-b [&>a]:pb-2 hover:[&>a]:border-lightBlue">
+                                    <div className="text-gray-200 relative pt-2 flex flex-col space-y-4 hover:[&>a]:text-gray-400 [&>a]:border-b [&>a]:pb-2 hover:[&>a]:border-gray-400">
                                         <Link onClick={() => { handleOpen(); handleHidden() }} href="/about" >Our Church</Link>
                                         <Link onClick={() => { handleOpen(); handleHidden() }} href="/about/beliefs" >Our Beliefs</Link>
                                     </div>
@@ -117,16 +120,15 @@ export default function Header() {
                         <Link onClick={() => { handleOpen(); handleHidden() }} href="/resources">Resources</Link>
 
                         <div onClick={handleMedia} className="cursor-pointer">
-                            <div className="hover:text-lightBlue flex items-center space-x-1">
+                            <div className="hover:text-gray-400 flex items-center space-x-1">
                                 <span>Media</span>
-                                <span>{isMediaClosed ? "+" : "-"}</span>
+                                <span>{isMediaClosed ? <MdOutlineKeyboardArrowUp /> : <MdOutlineKeyboardArrowDown />}</span>
                             </div>
                             {isMediaClosed ? '' :
                                 <div className="z-50">
-                                    <div className="relative pt-2 flex flex-col space-y-3 shadow-md hover:[&>a]:text-lightBlue [&>a]:border-b [&>a]:pb-2 hover:[&>a]:border-lightBlue">
+                                    <div className="relative pt-2 flex flex-col space-y-3 shadow-md hover:[&>a]:text-gray-400 [&>a]:border-b [&>a]:pb-2 hover:[&>a]:border-gray-400">
                                         <Link onClick={() => { handleOpen(); handleHidden() }} href="/media" >Our Music</Link>
                                         <Link onClick={() => { handleOpen(); handleHidden() }} href="/media/sermons" >Live Sermons</Link>
-                                        <Link onClick={() => { handleOpen(); handleHidden() }} href="/media/gallery" >Photo Gallery</Link>
                                     </div>
                                 </div>}
                         </div>
