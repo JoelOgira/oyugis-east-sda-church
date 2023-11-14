02e9ac687fb894ea6038cb5e3213f87013d04485
Jay <joelogira@gmail.com>
2023-11-14 08:04:29 +0300

    Added metadata api

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,11 @@ import SermonsSnippet from "./components/SermonsSnippet";
 import Showcase from "./components/Showcase";
 import Tithes from "./components/Tithes";
 
+export const metadata = {
+  title: 'Oyugis East Sda Church',
+  description: 'Explore the welcoming community of Oyugis East Seventh-day Adventist Church. Discover our beliefs, activities, and events. Join us in worship and fellowship.',
+}
+
 export default function Home() {
   return (
     <main className="min-h-screen">
