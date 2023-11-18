af2baa54136b933f04d583b00be36e5f086f5e94
Jay <joelogira@gmail.com>
2023-11-18 20:46:09 +0300

    Created our-beliefs component

---
+++ app/about/our-beliefs/page.jsx
@@ -0,0 +1,41 @@
+import { beliefs } from "../import"
+
+export const metadata = {
+    title: 'Oyugis East Sda Church | Beliefs',
+    description: 'Our 28 fundamental beliefs that outline the core doctrines and beliefs of our denominations.',
+}
+
+export default function OurBeliefs() {
+    return (
+        <div className="w-full">
+            <div className="bg-gray-100">
+                <div className="px-5 py-8 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4">
+
+                    {/* <div className="w-full pb-4">
+                        <SearchBeliefs search={search} setSearch={setSearch} />
+                    </div> */}
+
+                    <p className="leading-8 text-justify pb-8 lg:text-lg">
+                        Seventh-day Adventists accept the Bible as their only creed and hold certain fundamental beliefs to be the teaching of the Holy Scriptures. These beliefs, as set forth here, constitute the church’s understanding and expression of the teaching of Scripture. Revision of these statements may be expected at a General Conference session when the church is led by the Holy Spirit to a fuller understanding of Bible truth or finds better language in which to express the teachings of God’s Holy Word.
+                    </p>
+                    <div className="flex flex-col space-y-5 [&>div>div]:bg-gray-300">
+
+                        {(beliefs || []).map(belief => (
+                            <div key={belief?.id} className="flex flex-col space-y-3">
+                                <h3 className="text-tertiary font-bold text-2xl">
+                                    {belief?.title}
+                                </h3>
+                                <div className="border border-l-8 border-l-tertiary p-8">
+                                    <p className="leading-8">
+                                        {belief?.belief}
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
