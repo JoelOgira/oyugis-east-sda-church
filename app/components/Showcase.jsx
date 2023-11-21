8c5dbd7af56c3a92ce33ba6b8ed011e721ea737b
Jay <joelogira@gmail.com>
2023-11-21 19:15:59 +0300

    Updated content and styling for showcase component

--- app/components/Showcase.jsx
+++ app/components/Showcase.jsx
@@ -6,13 +6,10 @@ export default function Showcase() {
   return (
     <div className="w-full showcase-bg">
       <div className="bg-gradient">
-        <div className="px-3 py-12 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4" >
-          <h2 className='text-center text-white font-bold text-xl pb-8 max-w-sm mx-auto lg:text-2xl lg:max-w-lg'>
-            Welcome to Seventh Day Adventist Church Oyugis East
-          </h2>
+        <div className="px-3 py-16 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4" >
           <div className="flex flex-col-reverse md:flex-row md:space-x-10 md:space-y-0">
-            <div className="flex flex-col space-y-6 justify-center mt-8 items-center text-center md:justify-start md:mt-0 md:items-start md:text-start md:w-1/2 lg:grid lg:items-center">
-              <p className='font-semibold text-gray-100 max-w-sm lg:leading-8'>
+            <div className="flex flex-col space-y-6 justify-center mt-8 items-center text-center [&>p]:text-justify sm:[&>p]:text-center md:[&>p]:text-start md:justify-start md:mt-0 md:items-start md:text-start md:w-1/2 lg:grid lg:items-center">
+              <p className='font-semibold text-gray-100 max-w-lg md:max-w-sm lg:leading-8'>
                 Seventh Day Adventist (SDA) Oyugis East Church is part of a world wide community of Seventh Day Adventist Christians
               </p>
               <p className='text-gray-200 text-sm md:text-base lg:max-w-lg lg:leading-8'>
