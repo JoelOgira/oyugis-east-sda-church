9552f18214ab80351b95dfdc946c54046c47520d
Jay <joelogira@gmail.com>
2023-11-29 21:38:34 +0300

    Updated the styling in the component

--- app/about/our-beliefs/page.jsx
+++ app/about/our-beliefs/page.jsx
@@ -15,24 +15,22 @@ export default function OurBeliefs() {
                         <SearchBeliefs search={search} setSearch={setSearch} />
                     </div> */}
 
-                    <p className="leading-8 text-justify pb-8 lg:text-lg">
+                    <p className="leading-6 text-justify pb-8 lg:text-lg">
                         Seventh-day Adventists accept the Bible as our only creed and hold certain fundamental beliefs to be the teaching of the Holy Scriptures. These beliefs, as set forth here, constitute the church’s understanding and expression of the teaching of Scripture. Revision of these statements may be expected at a General Conference session when the church is led by the Holy Spirit to a fuller understanding of Bible truth or finds better language in which to express the teachings of God’s Holy Word.
                     </p>
-                    <div className="flex flex-col space-y-5 [&>div>div]:bg-gray-300">
-
+                    <div className="grid grid-cols gap-5 [&>div>div]:bg-gray-300">
                         {(beliefs || []).map(belief => (
                             <div key={belief?.id} className="flex flex-col space-y-3">
                                 <h2 className="font-bold text-2xl">
                                     {belief?.title}
                                 </h2>
-                                <div className="border border-l-8 border-l-tertiary p-8">
-                                    <p className="leading-8">
+                                <div className="border border-l-8 border-l-tertiary p-6">
+                                    <p className="leading-6">
                                         {belief?.belief}
                                     </p>
                                 </div>
                             </div>
                         ))}
-
                     </div>
                 </div>
             </div>
