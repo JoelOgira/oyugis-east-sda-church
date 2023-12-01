52f256d1210e45009252a4fa77426a4a1e338975
Jay <joelogira@gmail.com>
2023-12-01 07:21:06 +0300

    Modified the styling

--- app/about/our-beliefs/page.jsx
+++ app/about/our-beliefs/page.jsx
@@ -10,12 +10,7 @@ export default function OurBeliefs() {
         <div className="w-full">
             <div className="bg-gray-100">
                 <div className="px-5 py-8 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4">
-
-                    {/* <div className="w-full pb-4">
-                        <SearchBeliefs search={search} setSearch={setSearch} />
-                    </div> */}
-
-                    <p className="leading-6 text-justify pb-8 lg:text-lg">
+                    <p className="leading-6 text-sm text-justify pb-8 lg:text-lg">
                         Seventh-day Adventists accept the Bible as our only creed and hold certain fundamental beliefs to be the teaching of the Holy Scriptures. These beliefs, as set forth here, constitute the church’s understanding and expression of the teaching of Scripture. Revision of these statements may be expected at a General Conference session when the church is led by the Holy Spirit to a fuller understanding of Bible truth or finds better language in which to express the teachings of God’s Holy Word.
                     </p>
                     <div className="grid grid-cols gap-5 [&>div>div]:bg-gray-300">
@@ -25,7 +20,7 @@ export default function OurBeliefs() {
                                     {belief?.title}
                                 </h2>
                                 <div className="border border-l-8 border-l-tertiary p-6">
-                                    <p className="leading-6">
+                                    <p className="leading-6 text-sm">
                                         {belief?.belief}
                                     </p>
                                 </div>
