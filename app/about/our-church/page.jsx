fd8ef346a716ce6e0f2f82e014c20a4d155f976b
Jay <joelogira@gmail.com>
2023-12-01 07:22:16 +0300

    Added tracking-tight styling to the <p> tags

--- app/about/our-church/page.jsx
+++ app/about/our-church/page.jsx
@@ -13,7 +13,7 @@ export default function OurChurch() {
                 <div className="flex justify-center items-center h-full text-center">
                     <div className="flex items-center flex-col space-y-3">
                         <h2 className="font-semibold text-lg uppercase text-tertiary">Mission Statement</h2>
-                        <p className="text-justify text-sm max-w-3xl leading-6 md:text-base">
+                        <p className="text-justify tracking-tight text-sm max-w-3xl leading-6 md:text-base">
                             Our Mission as Oyugis East Sda Church is - "Make Disciples of all people, communicating the everlasting gospel in the context of the three angel's messages of Revelation 14:6-12, leading them to accept Jesus as personal saviour and unite with His remnant Church, discipling them to serve Him as Lord, and prepare tem for His soon coming."
                         </p>
                     </div>
@@ -24,7 +24,7 @@ export default function OurChurch() {
                 <div className="flex justify-center items-center h-full text-center">
                     <div className="flex items-center flex-col space-y-3">
                         <h2 className="font-semibold text-lg uppercase text-tertiary">Vision</h2>
-                        <p className="text-justify text-sm max-w-3xl leading-6 md:text-base">
+                        <p className="text-justify tracking-tight text-sm max-w-3xl leading-6 md:text-base">
                         Our vision as Oyugis East Sda Church is - "In harmony with the great prophecies of the Scriptures, we see as the climax of God's plan, the restoration of all His creation to full Harmony with His perfect will."
                         </p>
                     </div>
